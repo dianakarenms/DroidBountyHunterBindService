3b161d3edaa5285e98724a12e1ab58480bd50dd9
karenmunoz <dianakarenms@gmail.com>
2018-05-23 16:55:28 -0500

    Initial commit

---
+++ app/src/main/java/edu/training/droidbountyhunterbindservice/MainActivity.java
@@ -0,0 +1,98 @@
+package edu.training.droidbountyhunterbindservice;
+
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /** Messenger for communicating with the service. */
+    Messenger mService = null;
+
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mBound;
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            mService = new Messenger(service);
+            mBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mBound = false;
+        }
+    };
+
+    public void sayHello(View v) {
+        if (!mBound) return;
+        // Create and send a message to the service, using a supported 'what' value
+        Message msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to the service
+        bindService(new Intent(this, MessengerService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bind_fugitives:
+                Toast.makeText(this, "Bind clicked", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(this, MessengerService.class);
+//                bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+                Intent i = new Intent();
+                i.setComponent(new ComponentName("edu.training.droidbountyhunter", "edu.training.services.MessengerService"));
+                getApplication().bindService(i, mConnection, Context.BIND_AUTO_CREATE);
+                break;
+
+            case R.id.list_fugitives:
+                Toast.makeText(this, "List clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
